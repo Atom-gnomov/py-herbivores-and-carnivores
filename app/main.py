bae631882a40b1e23cd07e8f3a60a8676af79d1c
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-05 18:26:43 +0200

    complete

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        bcw = self.name
+        acw = f"{{Name: {bcw}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return acw
+
+    @staticmethod
+    def print_alive() -> str:
+        return str(Animal.alive)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other: Herbivore) -> None:
+        if not other.hidden and isinstance(other, Herbivore):
+            other.health -= 50
+            if other.health <= 0:
+                Animal.alive.remove(other)
